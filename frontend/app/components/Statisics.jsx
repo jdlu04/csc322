1e560246b97e63bde614217ef742e81bae9608f4
jdlu04 <liujudy04@gmail.com>
2025-04-27 17:02:08 -0400

    paid home components

--- frontend/app/components/Statisics.jsx
+++ frontend/app/components/Statisics.jsx
@@ -2,10 +2,16 @@ import React from "react";
 
 export default function Statisics() {
   return (
-    <div className="h-1/2 w-3/4 pb-10 text-textGrey">
+    <div className="h-20 w-3/4 mb-5 text-textGrey">
       <div className="h-10 w-full border-l border-r border-t rounded-t-lg bg-white">
-        <p>Textbox</p>
+        <p>Statistics</p>
       </div>
+      <div className="h-14 w-full border rounded-b-lg bg-white inline-flex">
+        <p> Edited Texts: </p>
+        <p className="px-10"> Used Tokens: </p>
+        <p> Corrections: </p>
+      </div>
+      
     </div>
   );
 }
