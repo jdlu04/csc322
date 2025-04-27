45873e5144811bff0cb47860ba0363199427ae4e
jdlu04 <liujudy04@gmail.com>
2025-04-27 12:02:13 -0400

    free user components

---
+++ frontend/app/components/CorrectionCheckbox.jsx
@@ -0,0 +1,35 @@
+'use client';
+
+import React, { useState } from 'react'
+
+export default function CorrectionCheckbox() {
+    const [isSelfChecked, setSelfChecked] = useState(false);
+    const [isLLMChecked, setLLMChecked] = useState(false);
+
+    const handleLLMChecked = () => {
+        setLLMChecked(!isLLMChecked)
+        setSelfChecked(false)
+    }
+    
+    const handleSelfChecked = () => {
+        setSelfChecked(!isSelfChecked)
+        setLLMChecked(false)
+    }
+
+    return (
+    <div className='flex items-center'>
+        <input
+            type='checkbox'
+            checked={isLLMChecked}
+            onChange={handleLLMChecked}
+        />
+        <p className='text-black'>LLM Correction</p>
+        <input
+            type='checkbox'
+            checked={isSelfChecked}
+            onChange={handleSelfChecked}
+        />
+        <p className='text-black'>Self Correction</p>
+    </div>
+  )
+}
--- frontend/app/components/CorrectionTextBox.jsx
+++ frontend/app/components/CorrectionTextBox.jsx
@@ -2,14 +2,26 @@ import React from 'react'
 
 export default function CorrectionTextBox() {
   return (
-    <div className='h-1/2 w-3/4 bg-white text-textGrey border rounded-lg'>
-        <div className='h-10 w-full border-b'>
-            Textbox
-            {/*Available tokens */}
+    <div className='h-1/2 w-3/4 pb-10 text-textGrey'>
+        <div className='h-10 w-full border-l border-r border-t rounded-t-lg bg-white justify-between inline-flex'>
+            <p>Textbox</p>
+            <p>Available Tokens:</p>
         </div>
-        <div className='w-full h-full'>
-            <textarea className=' resize-none size-full'></textarea>
-            {/*Blacklist words, upload text, and save text */}
+        <div className='w-full h-full border rounded-b-lg bg-white'>
+            <textarea className='resize-none w-full h-11/12'/>
+            
+            <div className='w-full inline-flex'>
+                {/*if user is paid -> save text button*/}
+                <button className='text-textGrey'>
+                    blacklist
+                </button>
+                <p> | </p>
+                <button className='text-textGrey'>
+                    upload text
+                </button>
+            </div>
+           
+            
         </div>
     </div>
   )
