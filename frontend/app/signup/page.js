03bab6a313641f4ce562fec0e09950d548123b4e
jdlu04 <liujudy04@gmail.com>
2025-05-11 20:11:00 -0400

    signup UI

---
+++ frontend/app/signup/page.js
@@ -0,0 +1,48 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import Dropdown from "@/components/Dropdown";
+
+export default function page() {
+  const items = ["Free User", "Paid User", "Super User"];
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/paid");
+  };
+
+  return (
+    <div className="h-screen">
+      <div className="h-3/4 border rounded-2xl bg-white border-boxBorder text-lg flex flex-col items-center justify-center w-full">
+        <h1 className="text-4xl font-semibold text-black">Sign Up</h1>
+        <div className="h-20 w-5/6">
+          <p className="text-textGrey">Username</p>
+          <input className="border rounded-lg h-12 w-full border-boxBorder"></input>
+        </div>
+        <div className="h-20 w-5/6 my-5">
+          <p className="text-textGrey">Password</p>
+          <input className="border rounded-lg h-12 w-full border-boxBorder"></input>
+        </div>
+        <div className="h-48 w-5/6">
+          <p className="text-textGrey">User Type</p>
+          <Dropdown options={items}></Dropdown>
+        </div>
+        <button
+          className="bg-accentGreen w-3/4 h-14 rounded-4xl"
+          onClick={handleClick}
+        >
+          <p className="font-semibold">Sign Up</p>
+        </button>
+      </div>
+      <div className="border my-10 rounded-2xl bg-white border-boxBorder text-lg flex items-center justify-center w-full h-20">
+        <div className="text-black flex items-center space-x-2 text-lg font-semibold">
+          <p>Have an account?</p>
+          <Link href="/login" className="underline">
+            Login
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ frontend/components/Dropdown.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+function Dropdown({ options, onSelect }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedValue, setSelectedValue] = useState(null);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOptionClick = (option) => {
+    setSelectedValue(option);
+    //onSelect(option);
+    setIsOpen(false);
+  };
+
+  return (
+    <div className="text-black w-full">
+      <button
+        onClick={toggleDropdown}
+        className="border rounded-lg h-12 w-full border-boxBorder text-left"
+      >
+        {selectedValue || "Select an option"}
+      </button>
+      {isOpen && (
+        <ul className="border-l border-r border-boxBorder rounded">
+          {options.map((option) => (
+            <li className="border-b border-boxBorder"key={option} onClick={() => handleOptionClick(option)}>
+              {option}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default Dropdown;
