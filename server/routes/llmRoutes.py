788970c760ff1e104dd579931c5edc47cdb75f26
cwang006 <cwang006.ccny@gmail.com>
2025-05-14 15:57:23 -0400

    Added /save-file endpoint

--- server/routes/llmRoutes.py
+++ server/routes/llmRoutes.py
@@ -14,7 +14,7 @@ load_dotenv()
 mongoURL = os.getenv("DB_URL")
 client = MongoClient(mongoURL, tlsAllowInvalidCertificates=True)
 db = client["TIFIdb"]
-# users_col = db["free_users"] # temporarily using since there are 2 DBs for users? 
+users_col = db["users"] # temporarily using since there are 2 DBs for users?
 token_col = db["tokens"]
 text_col = db["textupload"] # temp db for holding uploaded LLM text -> will be cleaning/updating
 
@@ -81,7 +81,41 @@ def review_text():
     return jsonify(result), 200
 
 # Endpoint for Accepting a specific correction (deducted 1 token)
-#@llm_bp.route('/llm-correct/accept', methods=['POST'])
+# @llm_bp.route('/llm-correct/accept', methods=['POST'])
+# def llm_accept():
+#    data = request.json
 
 # Endpoint for Rejecting a specific correction, submitting a reason for super user to review
 #@llm_bp.route('/llm-correct/reject', methods=['POST'])
+
+# Endpoint for saving wrongly flagged word as "correct" --> ?
+# @llm_bp.route('/llm-correction/save-as-correct', methods=['POST'])
+# def save_correct():
+
+# Endpoint for saving file 
+@llm_bp.route('/save-file', methods=['POST'])
+def saveFile():
+    data = request.json()
+    username = data['username']
+    text = data['text']
+
+    # ----> need to check if they are a paid user first;
+    # Logic: check db["users"] & then userType="Paid User"; if not, then return error
+    if not data or 'username' not in data:
+        return jsonify({"Error":"Username required"}), 400
+    user = users_col.find_one({"username":username})
+    if not user:
+        return jsonify({"Error":"Username not found"}), 404
+    
+    userType = user.get("")
+    
+    if not text:
+        return jsonify({"Error":"Text required"}), 400
+    
+    token_count = len(re.findall(r'\S+', text))
+    avaliable_tokens = user.get("tokenBalance", 0)
+    remaining_tokens = avaliable_tokens - token_count
+    
+    # Check if the required token_count of text and avaliable token count of user difference
+    if remaining_tokens < 5:
+        return jsonify({"Error":"Not enough tokens in balance"})
