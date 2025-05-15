61d687d72d669fdd1e1853ca071db18e9b66aa89
jdlu04 <liujudy04@gmail.com>
2025-05-15 08:19:35 -0400

    boxes on boxes

--- frontend/app/paid/page.js
+++ frontend/app/paid/page.js
@@ -1,10 +1,12 @@
 "use client";
+
 import { React, useState } from "react";
 import CorrectionTextBox from "../../components/CorrectionTextBox";
 import CorrectionCheckbox from "../../components/CorrectionCheckbox";
 import Statisics from "../../components/Statisics";
 export default function page() {
-  const [texts, setText] = useState("");
+  const [correctedText, setCorrectedText] = useState("");
+  const [originalText, setOriginalText] = useState("");
   const [llmCheck, setLLMCheck] = useState(false);
 
   const handleSubmit = () => {
@@ -15,7 +17,7 @@ export default function page() {
       //self correction
     }
   };
-  
+
   const llmCorrection = async () => {
     console.log("llm function");
     try {
@@ -24,25 +26,27 @@ export default function page() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ text: texts }),
+        body: JSON.stringify({ text: correctedText }),
       });
-  
+
       if (!response.ok) {
-        const errorText = await response.text(); 
+        const errorText = await response.text();
         console.error("Server error response:", errorText);
         alert("Something went wrong. See console for details.");
         return;
       }
-  
+
       const result = await response.json();
-  
-      setText(result.corrected);
+
+      setCorrectedText(result.corrected);
+      setOriginalText(result.original);
+
+      console.log("Original:", result.original);
       console.log("Corrected:", result.corrected);
     } catch (error) {
       console.error(error);
     }
   };
-  
 
   return (
     <div className="w-screen h-screen bg-greyBG">
@@ -61,11 +65,41 @@ export default function page() {
           setLLMCheck(isLLMSelected);
         }}
       />
-      <CorrectionTextBox value={texts}
-  onTextChange={(text) => setText(text)}/>
-      <button className="h-10 w-25 bg-black" onClick={handleSubmit}>
-        Submit
-      </button>
+      <div className="justify-between inline-flex h-screen w-screen">
+        <div className="w-1/2 h-1/2">
+          <div className="h-screen w-screen pb-10 text-textGrey">
+            <div className="h-10 w-1/2 border-l border-r border-t rounded-t-lg bg-white justify-between inline-flex">
+              <p>Textbox</p>
+              <p>Available Tokens:</p>
+            </div>
+            <div className="w-1/2 h-1/2 border rounded-b-lg bg-white">
+              <textarea
+                className="resize-none w-full h-11/12"
+                value={correctedText}
+                onChange={(e) => setCorrectedText(e.target.value)}
+              />
+              <div className="w-1/2 inline-flex">
+                <button className="text-textGrey">blacklist</button>
+                <p> | </p>
+                <button className="text-textGrey">upload text</button>
+              </div>
+            </div>
+            <button className="h-10 w-25 bg-black my-5" onClick={handleSubmit}>
+              Submit
+            </button>
+          </div>
+        </div>
+        <div className="w-1/2">
+          <div className="h-screen w-full px-2 pb-8 text-textGrey">
+            <div className="h-10 w-3/4 border-l border-r border-t rounded-t-lg bg-white justify-between inline-flex">
+              <p>Textbox</p>
+            </div>
+            <div className="w-3/4 h-1/2 border rounded-b-lg bg-white">
+              <div></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
